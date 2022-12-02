0c81670da4718f195768b84bf7f23f38e0d88195
Danieeeel23 <hidalgodaniellois0@gmail.com>
2022-12-02 09:27:25 +0800

    student notification

--- Student/Create_Excuse_letter.php
+++ Student/Create_Excuse_letter.php
@@ -45,31 +45,31 @@ if(!isset($_SESSION['student_id'])){
                 </li>
                 <li>
                     <a href="Lists_of_Concerns.php">
-                    <span class="icon"><img src="images/sidebar_menu/Concerns.svg" alt=""></span>
+                    <span class="icon"><img src="images/sidebar_menu/Concerns.svg" alt=""></span><span class="badge2 badge-primary">4</span>
                     <span class="title2" ><br>Concerns</span>
                     </a>
                 </li>
                 <li>
                     <a href="Lists_of_Excuse_Letter.php">
-                    <span class="icon"><img src="images/sidebar_menu/Excuse_slip.svg" alt=""></span>
+                    <span class="icon"><img src="images/sidebar_menu/Excuse_slip.svg" alt=""></span><span class="badge2 badge-primary">4</span>
                     <span class="title" ><br>Excuse Letter</span>
                     </a>
                 </li>
                 <li>
                     <a href="Lists_of_Violation.php">
-                    <span class="icon"><img src="images/sidebar_menu/Mask group (7).svg" alt=""></span>
+                    <span class="icon"><img src="images/sidebar_menu/Mask group (7).svg" alt=""></span><span class="badge2 badge-primary">4</span>
                     <span class="title3" ><br>Violation</span>
                     </a>
                 </li>
                 <li>
                     <a href="Lists_of_Failing_Grades.php">
-                    <span class="icon"><img src="images/sidebar_menu/Mask group (8).svg" alt=""></span>
+                    <span class="icon"><img src="images/sidebar_menu/Mask group (8).svg" alt=""></span><span class="badge2 badge-primary">4</span>
                     <span class="title4" ><br>Failing Grades</span>
                     </a>
                 </li>
                 <li>
                     <a href="Lists_of_Announcement.php">
-                    <span class="icon"><img src="images/sidebar_menu/Mask group (9).svg" alt=""></span>
+                    <span class="icon"><img src="images/sidebar_menu/Mask group (9).svg" alt=""></span><span class="badge2 badge-primary">4</span>
                     <span class="title5" ><br>Announcement</span>
                     </a>
                 </li>
