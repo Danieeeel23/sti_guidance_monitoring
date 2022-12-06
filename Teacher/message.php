54b5d837cb54c581bc0c9bff75766adad4fccd7b
kurty430 <kurtdex430@gmail.com>
2022-12-07 01:28:51 +0800

    Update message.php

--- Teacher/message.php
+++ Teacher/message.php
@@ -1,44 +1,57 @@
 <?php
-    if(isset($_SESSION['message'])) :
+if (isset($_SESSION['message'])) :
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <!DOCTYPE html>
+  <html>
 
-<style>
-.alert {
-  position: absolute;
-  margin: 0 190px 0 300px;
-  padding: 20px;
-  width: 800px;
-  background-color: #B2C8DF;
-  color: black;
-  border-radius: 10px;
-}
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-</style>
-</head>
-<body>
+    <style>
+      .alert-container {
+        width: 100%;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
 
-<div class="alert" id="alert">
-  <span onclick="this.parentElement.style.display='none';"></span> 
-  <strong>Hey!</strong> <?= $_SESSION['message']; ?>
-</div>
+      .alert {
+        /* margin: 0 190px 0 300px; */
+        width: 70%;
+        padding: 2rem;
+        background-color: #B2C8DF;
+        color: black;
+        z-index: 2;
+        border-radius: 10px;
+        font-size: 1.5rem;
+        text-align: center;
+        margin-top: 1rem;
+      }
+    </style>
+  </head>
 
-<!-- automatically close alert message -->
-<script type="text/javascript">
-    $("#alert").show().delay(3000).fadeOut();
-</script>
+  <body>
+    <div class="alert-container" id="alert">
+      <div class="alert">
+        <span onclick="this.parentElement.style.display='none';"></span>
+        <strong>Hey!</strong> <?= $_SESSION['message']; ?>
+      </div>
+    </div>
+    <!-- automatically close alert message -->
+    <script type="text/javascript">
+      $("#alert").show().delay(2000).fadeOut();
+    </script>
 
-</body>
-</html>
+  </body>
+
+  </html>
 
 
-<?php 
-    unset($_SESSION['message']);
-    endif;
+<?php
+  unset($_SESSION['message']);
+endif;
 ?>
